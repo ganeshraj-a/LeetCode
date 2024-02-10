3ec5adef24316db2004a39775f2069457f935013
ganeshraj-a <79321086+ganeshraj-a@users.noreply.github.com>
2024-02-10 08:00:37 +0530

    Create : palindromic-substrings.py
    10/02/2024 - Leetcode - DailyChallenge

---
+++ Daily challenge/palindromic-substrings.py
@@ -0,0 +1,33 @@
+"""Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+ 
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa"."""
+
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n = 0
+        for i in range(len(s)):
+            for j in range(i,len(s)+1):
+                if s[i:j] == s[i:j][::-1] and s[i:j] != '':
+                    # print(s[i:j])
+                    n += 1 
+
+
+                    # a a a a aa  aa aa aaa aaa aaaa
+        return n
+        
